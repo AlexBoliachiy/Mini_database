956545c53256f6c680dd229d42fecd27bd4827f5
AlexBoliachiy <parkerrulit@gmail.com>
2016-12-01 09:07:03 +0200

    test's updated

--- main_test.go
+++ main_test.go
@@ -36,6 +36,31 @@ func Test(t *testing.T) {
 		t.Error("select return wrong string")
 
 	}
+	time.Sleep(100 * time.Millisecond)
+
+	fmt.Fprintf(conn, "update 222 to 10 in test_table\n")
+	time.Sleep(100 * time.Millisecond)
+	fmt.Fprintf(conn, "select 222 from test_table\n")
+
+	message, _ = bufio.NewReader(conn).ReadString('\n')
+	message = strings.Replace(strings.Replace(message, " ", "", -1), "\n", "", -1)
+	if message != "10" {
+		t.Error("update works wrong")
+
+	}
+	time.Sleep(100 * time.Millisecond)
+
+	fmt.Fprintf(conn, "delete 222 in test_table\n")
+	time.Sleep(100 * time.Millisecond)
+	fmt.Fprintf(conn, "select 222 from test_table\n")
+
+	message, _ = bufio.NewReader(conn).ReadString('\n')
+	message = strings.Replace(strings.Replace(message, " ", "", -1), "\n", "", -1)
+	if message != "" {
+		t.Error("delete works wrong")
+
+	}
+
 	fmt.Fprintf(conn, "exit 1")
 
 	fmt.Println("OK")
